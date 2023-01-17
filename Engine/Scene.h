779f215dc5acd8499b59a445f3adbba65f25e28b
audreyamar <71273052+audreyamar@users.noreply.github.com>
2023-01-17 15:34:00 +0100

    ADD: Engine directory

---
+++ Engine/Scene.h
@@ -0,0 +1,18 @@
+//
+// Created by aaama on 17/01/2023.
+//
+
+#ifndef B_CPP_500_REN_5_2_RTYPE_AUDREY_AMAR_SCENE_H
+#define B_CPP_500_REN_5_2_RTYPE_AUDREY_AMAR_SCENE_H
+
+
+class Scene {
+    std::vector<Entity>;
+    std::vector<ISystem>;
+
+    void update(Engine);
+
+};
+
+
+#endif //B_CPP_500_REN_5_2_RTYPE_AUDREY_AMAR_SCENE_H
