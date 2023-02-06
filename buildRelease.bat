ce19ecce08d61bb4bb703c58d4fda75add733a15
Théo GUGUEN <72015839+theoguguen@users.noreply.github.com>
2023-02-06 17:40:17 +0100

    FEAT Bat script to build a release

---
+++ buildRelease.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+mkdir build
+cmake  -DCMAKE_MAKE_PROGRAM=ninja -G Ninja -S . -B .\build
+cd build
+ninja
+cd ..\
+mkdir release
+robocopy .\Client\assets release\assets /E
+copy .\build\r-type-client.exe release\
+powershell Compress-Archive release\* .\release\r-type-client.zip
+copy .\build\r-type-server.exe release\
+DEL .\release\r-type-client.exe
+@RD /S /Q .\release\assets
