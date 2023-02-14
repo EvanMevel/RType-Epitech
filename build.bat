5009fa950b2488f8073984378b6fc39fb793dc0f
audreyamar <71273052+audreyamar@users.noreply.github.com>
2023-02-14 11:17:14 +0100

    FEAT: github action (compile linux and windows) with raylib dependencies

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+git submodule update --recursive --remote
 mkdir build
 cmake  -DCMAKE_MAKE_PROGRAM=ninja -G Ninja -S . -B .\build
 cd build
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git submodule update --recursive --remote
 mkdir build
 cmake  -DCMAKE_MAKE_PROGRAM=ninja -G Ninja -S . -B ./build
 cd build
